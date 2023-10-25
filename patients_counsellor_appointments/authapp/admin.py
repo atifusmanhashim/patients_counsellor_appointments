b0388144d6a5108905b4dc70f357d140bc612915
Atif Usman <atifusman@gatron-novatex.com>
2023-10-25 19:35:30 +0500

    update django admin

--- patients_counsellor_appointments/authapp/admin.py
+++ patients_counsellor_appointments/authapp/admin.py
@@ -8,9 +8,26 @@ admin.site.register(LoginAnalytics)
 
 from appointmentsapp.models import (Patient,Counsellor,Appointment)
 
-admin.site.register(Patient)
-admin.site.register(Counsellor)
-admin.site.register(Appointment)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('patient_id', 'patient_code','patient_name','patient_email', 'is_active','create_date','modified_date','deleted_date')
+
+    search_fields = ['patient_id', 'patient_code','patient_name','patient_email']
+
+admin.site.register(Patient,PatientAdmin)
+
+class CounsellorAdmin(admin.ModelAdmin):
+    list_display = ('counsellor_id', 'counsellor_code','counsellor_name','counsellor_email', 'is_active','create_date','modified_date','deleted_date')
+
+    search_fields = ['counsellor_id', 'counsellor_code','counsellor_name','counsellor_email']
+admin.site.register(Counsellor,CounsellorAdmin)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('appointment_id', 'appointment_code','appointment_patient','appointment_counsellor', 'appointment_date','is_active','create_date','modified_date','deleted_date')
+
+    search_fields = ['appointment_id', 'appointment_code','appointment_patient__patient_name','appointment_counsellor__counsellor_name']
+
+
+
+admin.site.register(Appointment,AppointmentAdmin)
 
 from appapi.models import (AppAnalytics)
 
